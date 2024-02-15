9b3da22c0b0cb12ce21571ba802f6491aab576d0
AtharvaPingale <atharvapingale@gmail.com>
2024-02-15 13:21:22 -0500

    UI improvements

--- src/App.vue
+++ src/App.vue
@@ -1,102 +1,100 @@
 <template>
   <div class="container">
-    <h3>Object detection with Tensorflow</h3>
+    <h3>Object recognition with Tensorflow</h3>
     <div class="mb-3">
       <div v-if="!isStreaming">
         <button class="btn btn-light" @click="openCamera">Open Camera</button>
       </div>
       <div v-else class="">
-            <button
-              @click="stopStreaming"
-              class="btn btn-light"
-            >
-              Stop Streaming
-            </button>
-            <button
-              @click="snapshot"
-              class="btn btn-light"
-            >
-              Snapshot
-            </button>
-            <button
-        @click="detect"
-        class="btn btn-light" 
-      >
-        <span >Detect Object</span>
-      </button>
+        <button @click="stopStreaming" class="btn btn-light">Stop Streaming</button>
+        <button @click="snapshot" class="btn btn-light">Snapshot</button>
+        <button @click="detect" class="btn btn-light">
+          <span>Detect Object</span>
+        </button>
       </div>
-      
-    <div v-if="result.length > 0">
-            <p>{{ result[0].class }}</p>
-        </div>
+
+      <div v-if="result.length > 0">
+        <p>{{ result.map((x) => x.class).join(', ') }}</p>
+      </div>
     </div>
     <div class="image-container mb-3">
       <div class="border">
-        <video ref="videoRef" autoplay="true" width="50%" height="50%" id="videoElement"/>
+        <video ref="videoRef" autoplay="true" width="50%" height="50%" id="videoElement" />
       </div>
       <div class="border">
-        <img src="@/assets/tf_logo_social.png" class="image" ref="imgRef" alt="image" crossorigin="anonymous" id="imageElement">
+        <img
+          src="@/assets/tf_logo_social.png"
+          class="image"
+          ref="imgRef"
+          alt="image"
+          crossorigin="anonymous"
+          id="imageElement"
+        />
       </div>
     </div>
-      
   </div>
 </template>
 
 <script>
-import { ref, onMounted } from "vue";
-import { defineComponent } from 'vue';
+import { ref, onMounted, defineComponent } from 'vue'
 import '@tensorflow/tfjs-backend-cpu'
 import '@tensorflow/tfjs-backend-webgl'
 import * as cocoSsd from '@tensorflow-models/coco-ssd'
-const video = document.getElementById("videoElement");
-const img = document.getElementById("imageElement");
+const video = document.getElementById('videoElement')
+const img = document.getElementById('imageElement')
 export default defineComponent({
   name: 'App',
-  setup(){
-    const imgRef = ref(img);
-    
-    const videoRef = ref(video);
-    const isStreaming = ref(false);
-    const result = ref([]);
-    let model = undefined;
+  setup() {
+    const imgRef = ref(img)
+    const videoRef = ref(video)
+    const isStreaming = ref(false)
+    const isLoading = ref(true)
+    const result = ref([])
+    let model = undefined
+
     onMounted(() => {
       getModel()
     })
-    async function getModel(){
-      model = await cocoSsd.load();
+
+    async function getModel() {
+      model = await cocoSsd.load()
+      isLoading.value = false
     }
 
     async function detect() {
-      const img = imgRef.value;
-      const predictions = await model.detect(img);
-      result.value = predictions;
-      
-      console.log(predictions, img);
-    }  
+      const img = imgRef.value
+      const predictions = await model.detect(img)
+      result.value = predictions
+      if(!result.value.length) result.value.push({class:"No objects detected, please try again"}) 
+      console.log(predictions, img)
+    }
+
     async function openCamera() {
       if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({video: true}).then((stream)=>{
-          isStreaming.value = true;
-          videoRef.value.srcObject = stream;
+        navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+          isStreaming.value = true
+          videoRef.value.srcObject = stream
         })
       }
     }
+
     function stopStreaming() {
-      const stream = videoRef.value.srcObject;
-      const tracks = stream.getTracks();
-      tracks.map((track) => track.stop());
-      isStreaming.value = false;
+      const stream = videoRef.value.srcObject
+      const tracks = stream.getTracks()
+      tracks.map((track) => track.stop())
+      isStreaming.value = false
     }
+
     function snapshot() {
-      const canvas = document.createElement("canvas");
-      const ctx = canvas.getContext("2d");
-      
-      ctx.drawImage(videoRef.value, 0, 0,canvas.width,canvas.height);
-      const data = canvas.toDataURL("image/png");
-      imgRef.value.setAttribute("src", data);
+      const canvas = document.createElement('canvas')
+      const ctx = canvas.getContext('2d')
+
+      ctx.drawImage(videoRef.value, 0, 0, canvas.width, canvas.height)
+      const data = canvas.toDataURL('image/png')
+      imgRef.value.setAttribute('src', data)
       result.value = []
     }
-  
+
     return {
       imgRef,
       detect,
@@ -105,10 +103,10 @@ export default defineComponent({
       videoRef,
       openCamera,
       stopStreaming,
-      snapshot,
+      snapshot
     }
   }
-});
+})
 </script>
 
 <style>
@@ -119,24 +117,24 @@ export default defineComponent({
   text-align: center;
   margin-top: 50px;
 }
-.image-container{
+.image-container {
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 2rem;
-  .image{
+  .image {
     width: 50%;
     height: 50%;
   }
-  #imageElement{
+  #imageElement {
     width: 100%;
     height: 100%;
   }
-  #videoElement{
+  #videoElement {
     width: 100%;
     height: 100%;
     transform: rotateY(180deg);
-    -webkit-transform:rotateY(180deg); /* Safari and Chrome */
-    -moz-transform:rotateY(180deg); /* Firefox */
+    -webkit-transform: rotateY(180deg); /* Safari and Chrome */
+    -moz-transform: rotateY(180deg); /* Firefox */
   }
 }
 </style>
