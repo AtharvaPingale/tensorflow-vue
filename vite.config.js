3c8af26353a5668804b84450ddf8e507fae60269
AtharvaPingale <atharvapingale@gmail.com>
2024-02-15 00:55:33 -0500

    baseurl fixed

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
+  base: `/tensorflow-vue/`,
   plugins: [
     vue(),
     vueJsx(),
